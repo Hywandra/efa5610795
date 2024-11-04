07ac0eca11af18efcd576ef3b77e140b7789746b
hywandra-segdev <ivandra.santos@gmail.com>
2024-11-04 17:58:04 +0000

    Tarefa finalizada, pronta a entregar

--- AeropCrontol/AeropCrontol.cpp
+++ AeropCrontol/AeropCrontol.cpp
@@ -14,18 +14,113 @@ inteiro como identificador.Adicione outras características conforme achar necess
 #include <iostream>
 #include <Windows.h>
 #include <queue>
+#include <string>
 
 using namespace std;
 
-struct Aviao
-{
+struct Aviao {
     string nome;
-    int identif;
-    string modelo;
-
-    Aviao(string n)
+    int id;
 };
-int main()
-{
-    
+
+
+void listarNumero(queue<Aviao>& fila);
+void autorizarDescolagem(queue<Aviao>& fila);
+void adicionarAviao(queue<Aviao>& fila);
+void listarAviao(queue<Aviao> fila);
+void listarPrimeiro(queue<Aviao>& fila);
+
+int main() {
+    queue<Aviao> descolar;
+    int op;
+
+    do {
+        cout << "\nControle de Pista de Descolagem\n";
+        cout << "1 - Listar o número de aviões para descolar\n";
+        cout << "2 - Autorizar descolagem do primeiro avião\n";
+        cout << "3 - Adicionar avião à fila de espera\n";
+        cout << "4 - Listar todos os aviões em fila de espera\n";
+        cout << "5 - Listar as características do primeiro avião da fila\n";
+        cout << "0 - Sair\n";
+        cout << "Escolha uma opção: ";
+        cin >> op;
+
+        switch (op) {
+        case 1:
+            listarNumero(descolar);
+            break;
+        case 2:
+            autorizarDescolagem(descolar);
+            break;
+        case 3:
+            adicionarAviao(descolar);
+            break;
+        case 4:
+            listarAviao(descolar);
+            break;
+        case 5:
+            listarPrimeiro(descolar);
+            break;
+        case 0:
+            cout << "Sair\n";
+            break;
+        default:
+            cout << "Opção inválida. Tente novamente.\n";
+        }
+    } while (op != 0);
+
+    return 0;
 }
+
+
+void listarNumero(queue<Aviao>& fila) {
+    cout << "Número de aviões na fila para descolar: " << fila.size() << "\n";
+}
+
+void autorizarDescolagem(queue<Aviao>& fila) {
+    if (!fila.empty()) {
+        Aviao primeiro = fila.front();
+        cout << "Avião " << primeiro.nome << " (ID: " << primeiro.id << ") autorizado a descolar.\n";
+        fila.pop();
+    }
+    else {
+        cout << "Não há aviões na fila de descolagem.\n";
+    }
+}
+
+void adicionarAviao(queue<Aviao>& fila) {
+    Aviao novo;
+    cout << "Inserir nome do avião: ";
+    cin >> novo.nome;
+    cout << "Inserir identificador do avião: ";
+    cin >> novo.id;
+    fila.push(novo);
+    cout << "Avião " << novo.nome << " (ID: " << novo.id << ") adicionado à fila de espera.\n";
+}
+
+void listarAviao(queue<Aviao> fila) {
+    if (fila.empty()) {
+        cout << "Não há aviões em fila de espera.\n";
+    }
+    else {
+        cout << "Aviões em fila de espera:\n";
+        while (!fila.empty()) {
+            Aviao aviao = fila.front();
+            cout << "- " << aviao.nome << " (ID: " << aviao.id << ")\n";
+            fila.pop();
+        }
+    }
+}
+
+void listarPrimeiro(queue<Aviao>& fila) {
+    if (!fila.empty()) {
+        Aviao primeiro = fila.front();
+        cout << "Primeiro avião:\n";
+        cout << "Nome: " << primeiro.nome << "\n";
+        cout << "ID: " << primeiro.id << "\n";
+    }
+    else {
+        cout << "Não há aviões para descolar.\n";
+    }
+}
+
